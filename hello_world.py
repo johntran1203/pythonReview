795ba888884f4a2206abca38edf38c7053544144
johntran <tranjohn1211@gmail.com>
2022-08-25 21:26:11 -0700

    mileage example

---
+++ hello_world.py
@@ -0,0 +1,2 @@
+print('hell world!')
+print(6 * 8 / 2**3 - 2 * 2)
---
+++ mileage.py
@@ -0,0 +1,3 @@
+print('how many kilometers did you cycle today ')
+kms = float(input())/1.60934
+print(f'ok, you ran {round(kms,2)} miles')
